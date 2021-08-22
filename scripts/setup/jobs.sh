70fa9adc0d4230337ce72058d62b796a1f1324a6
Marc Sladek <marc@sladek.me>
2021-08-22 02:28:00 +0200

    fix jobs naming

--- scripts/setup/jobs.sh
+++ scripts/setup/jobs.sh
@@ -17,7 +17,7 @@ if $hasZFS; then
   sudo ln -s ~/stuff/scripts/jobs/zfs-health.sh /etc/cron.hourly/zfs-health
   for label in hourly daily weekly monthly; do
     sudo rm /etc/cron.${label}/zfs-auto-snapshot
-    sudo ln -s ~/stuff/scripts/jobs/zfs-snap.sh /etc/cron.${label}/zfs-snap
+    sudo ln -s ~/stuff/scripts/jobs/zfs-auto-snap.sh /etc/cron.${label}/zfs-auto-snap
   done
   sudo ln -s ~/stuff/private/config/$(hostname)/zfs-auto /etc/zfs-auto
 else
