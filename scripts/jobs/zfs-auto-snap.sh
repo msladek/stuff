83fc42a04c59002ecbe497c1b2bd594a3f4a6932
Marc Sladek <marc@sladek.me>
2021-08-22 13:59:52 +0200

    improve zfs-auto-snap for remote snapshotting

--- scripts/jobs/zfs-auto-snap.sh
+++ scripts/jobs/zfs-auto-snap.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# zfs-auto-snapshot command must be available
-command -v zfs-auto-snapshot &> /dev/null || exit 1
+sshx() {
+  [[ -z "$1" ]] && eval "${@:2}" || ssh -t -q -o "BatchMode=yes" "$1" "${@:2}"
+  return $?
+}
 
 # config directory must exists
 cfgdir=/etc/zfs-auto
-[[ -d "$cfgdir" ]] || exit 1 
+[[ -d "$cfgdir" ]] || exit 1
 
 # expecting script to be linked below /etc/cron.${label}
 label=$(basename $(dirname $0) | cut -d '.' -f2)
@@ -22,7 +24,8 @@ for param in "$@"; do
 done
 
 for path in ${cfgdir}/*; do
-  snap_dataset=''
+  host=''
+  dataset=''
   snap_hourly=false
   snap_daily=false
   snap_weekly=false
@@ -30,22 +33,23 @@ for path in ${cfgdir}/*; do
   snap_recursive=false
   snap_destroy_only=false
   source $path
-  [[ -z "$snap_dataset" ]] \
-    && snap_dataset=$(basename $path | sed 's/\\/\//')
+  [[ $dataset =~ ":" ]] \
+    && host=$(echo $dataset | cut -d ':' -f 1) \
+    && dataset=$(echo $dataset | cut -d ':' -f 2)
+  [[ -z "$dataset" ]] && continue
   snap_enable=snap_${label}
   snap_enable=${!snap_enable}
-  if [[ "$snap_enable" == true || "$snap_enable" -gt 0 ]]; then
-    opts="--quiet --syslog"
-    [[ "$debug" == true ]] && opts="--dry-run"
-    opts="${opts} --prefix=auto --label=${label}"
-    [[ "$snap_enable" -gt 0 ]] && opts="${opts} --keep=${snap_enable}"
-    [[ "$snap_recursive" == true ]] && opts="${opts} --recursive"
-    [[ "$snap_destroy_only" == true ]] && opts="${opts} --destroy-only"
-    echo "run: ${opts} "${snap_dataset}""
-    zfs-auto-snapshot ${opts} "${snap_dataset}"
-  else
-    echo "skip: ${label} ${snap_dataset}"
-  fi
+  [[ "$snap_enable" != true && ! "$snap_enable" -gt 0 ]] && continue
+  [[ "$debug" == true ]] && opts="--dry-run"
+  opts="${opts} --prefix=auto --label=${label}"
+  [[ "$snap_enable" -gt 0 ]] && opts="${opts} --keep=${snap_enable}"
+  [[ "$snap_recursive" == true ]] && opts="${opts} --recursive"
+  [[ "$snap_destroy_only" == true ]] && opts="${opts} --destroy-only"
+  echo "run: ${opts} ${host}:${dataset}"
+  pathSet="PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/bin:/sbin:/bin'"
+  checkCmd="command -v zfs-auto-snapshot &> /dev/null"
+  execCmd="zfs-auto-snapshot ${opts} ${dataset}"
+  sshx "$host" "${pathSet} && ${checkCmd} && ${execCmd}"
 done
 
 exit 0
