21d58258e9d5b631de26782fec783d341e562a3f
Marc Sladek <marc@sladek.me>
2021-11-11 04:45:59 +0100

    enpasscli+ use hashed challenge

--- scripts/admin/enpasscli+.sh
+++ scripts/admin/enpasscli+.sh
@@ -5,12 +5,11 @@
 # make sure enpasscli is available in the path
 command -v enpasscli >/dev/null || exit 1
 
+exit_code=0
+
 # path to the mpw store file
 [ -n "$mpw_store" ] || \
   mpw_store=/tmp/enp_mpw_$USER
-# just some random prefix to check if decryption worked
-[ -n "$mpw_prefix" ] || \ 
-  mpw_prefix="ZJEeAYXuvjIFV3Obn9xy"
 # iteration count for pbkdf2 key derivation function
 [ -n "$pbkdf2_iter" ] || \
   pbkdf2_iter="100000"
@@ -21,37 +20,32 @@ command -v enpasscli >/dev/null || exit 1
 # request pin if enabled and not already set
 [ -z "$ENP_PIN" ] \
   && read -s -p "Enter PIN (or omit): " ENP_PIN && echo
+pin_challenge=$(echo $ENP_PIN | sha224sum | head -c 50)
 
-# load mpw from store if file and pin is available
-[ -f "$mpw_store" ] && [ -n "$ENP_PIN" ] \
-  && decrypted=$(openssl enc -d $ssl_params -in $mpw_store -k $ENP_PIN 2>/dev/null | tr -d '\0') \
-  && prefix=$(echo $decrypted | cut -d ':' -f 1) \
-  && [ "$prefix" = "$mpw_prefix" ] \
-  && mpw=$(echo $decrypted | cut -d ':' -f 2)
-
-[ -n "$decrypted" ] && [ -z "$mpw" ] \
-  && echo "oops, PIN is wrong"
+# try loading mpw from store if file and pin is available
+if [ -f "$mpw_store" ] && [ -n "$ENP_PIN" ]; then
+  decrypted=$(openssl enc -d $ssl_params -in $mpw_store -k $ENP_PIN 2>/dev/null | tr -d '\0')
+  challenge=$(echo $decrypted | cut -d ':' -f 1)
+  if [ "$pin_challenge" = "$challenge" ]; then
+    mpw=$(echo $decrypted | cut -d ':' -f 2)
+  else
+    echo "oops, PIN is wrong"
+    exit_code=2
+  fi
+fi
 
 # request mpw if missing
-[ -z "$mpw" ] \
-  && read -s -p "Enter master password: " mpw && echo
+[ -z "$mpw" ] && read -s -p "Enter master password: " mpw && echo
 
-# execute enpasscli with set mpw
-[ -n "$mpw" ] \
-  && export MASTERPW=$mpw \
-  && enpasscli -vault=$HOME/.enpass-vault -keyfile=$HOME/.enpass-keyfile -sort "$@"
-status_code=$?
+# execute enpasscli with set mpw and write it to the store if unlock was successful and pin is available
 
-# write mpw to the store if unlock was successful and pin is available
-[ $status_code ] && [ -n "$ENP_PIN" ] \
-  && touch $mpw_store && chmod 600 $mpw_store \
-  && echo "${mpw_prefix}:${mpw}" | openssl enc -e $ssl_params -out $mpw_store -k $ENP_PIN
+if [ -n "$mpw" ]; then
+  export MASTERPW=$mpw
+  enpasscli -vault=$HOME/.enpass-vault -keyfile=$HOME/.enpass-keyfile -sort "$@"
+  exit_code=$? # BORKED: returned error code by enpasscli always 0
+  [ $exit_code ] && [ -n "$ENP_PIN" ] \
+    && touch $mpw_store && chmod 600 $mpw_store \
+    && echo "${pin_challenge}:${mpw}" | openssl enc -e $ssl_params -out $mpw_store -k $ENP_PIN
+fi
 
-[ ! $status_code ] && rm -f $mpw_store
-
-# make sure the mpw doesn't leak
-unset decrypted
-unset mpw
-unset MASTERPW
-
-exit $status_code
+exit $exit_code
