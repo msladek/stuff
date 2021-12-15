c3ccb305bf85c8f41cf638b607a3edb2ac9499a9
Marc Sladek <marc@sladek.me>
2021-12-15 10:55:37 +0100

    more aliases

--- config/user/bash/aliases.sh
+++ config/user/bash/aliases.sh
@@ -11,6 +11,8 @@ alias ln='ln -i'
 alias sudo='sudo '
 
 # some more ls aliases
+command -v lsd >/dev/null \
+  && alias ls='lsd'
 alias l='ls -Flh --color=auto'
 alias ll='ls -aFlh --color=auto'
 alias la='ls -AFlh --color=auto'
@@ -21,16 +23,33 @@ alias ...='cd ../../ && ll'
 alias ....='cd ../../../ && ll'
 alias .....='cd ../../../../ && ll'
 
-# other aliases
+# colorize grep output
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# colorize diff output
+command -v colordiff >/dev/null \
+  alias diff='colordiff'
+
+alias apt="sudo apt"
+alias aptitude="sudo aptitude"
+
+# give cat some wings
 command -v batcat >/dev/null \
   && alias bat='batcat' \
   && alias batp='bat -p' \
   && alias cat='batp'
-alias lsports='sudo netstat -tulpn'
+
+# other aliases
+alias vi='vim'
+alias lsports='sudo netstat -atulpn'
 alias untar='tar -zxvf'
 alias mkdir='mkdir -pv'
 alias rmdir='rmdir -pv'
 alias ducks='du -cks * | sort -rn | head -11'
+
+# tmux/screen simplifications
 command -v screen >/dev/null \
   && alias screenr='screen -RR'
 command -v tmux >/dev/null \
