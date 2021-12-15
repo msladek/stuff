9b7f558cec0921d6197b3049814e0e8e15b9b898
Marc Sladek <marc@sladek.me>
2021-12-15 10:54:42 +0100

    lsd install from github

--- scripts/setup/installs.sh
+++ scripts/setup/installs.sh
@@ -15,5 +15,23 @@ sudo aptitude -y install \
     net-tools netcat ethtool curl wget dnsutils iotop iftop openssh-client \
     debian-goodies debian-keyring gnupg dirmngr lsb-release ca-certificates \
     ntp git tree pv dstat bat vim rsync htop screen tmux sshfs smartmontools \
-    zip unzip unrar-free unp software-properties-common build-essential \
+    diff colordiff zip unzip unrar-free unp software-properties-common build-essential \
   | egrep -v "is already installed|Reading |Writing |Building |Initializing "
+
+function github-install-latest() {
+  echo -e "Installing $1 from github ..."
+  local release_url="https://github.com/$1/releases/latest"
+  local current_version=$(curl -s $release_url | grep -o -P '(?<=releases/tag/).*(?=\">)')
+  local download_url="$release_url/download/$(echo $2 | sed "s|%s|$current_version|g")"
+  local tmp_deb="$(mktemp)" \
+    && wget --no-verbose --show-progress --output-document "$tmp_deb" "$download_url" \
+    && sudo dpkg --skip-same-version -i "$tmp_deb"
+  local exit_code=$?
+  rm -f "$tmp_deb"
+  return $exit_code
+}
+
+github-install-latest 'Peltoche/lsd' 'lsd-musl_%s_amd64.deb'
+
+unset github-install-latest
+
