7c148361f488a6978e1d98a7d88569fda8bc2b87
Marc Sladek <marc@sladek.me>
2022-01-25 12:35:01 +0100

    improve ssh setup

--- scripts/setup/ssh.sh
+++ scripts/setup/ssh.sh
@@ -3,20 +3,21 @@
 echo -e "\nSetup SSH ..."
 
 echo "... setup home directory"
-SSH_DIR=$HOME/.ssh
-install -d -m 700 -o $USER -g $(id -gn) $SSH_DIR
+sshDir=$HOME/.ssh
+install -d -m 700 -o $USER -g $(id -gn) $sshDir
 
-echo "... setup ssh private"
-PRIV_DIR=$HOME/stuff/private
-if [ -d $PRIV_DIR ]; then
-  ln -sfn $PRIV_DIR/ssh/config.d $SSH_DIR/config.d
-  ln -s $PRIV_DIR/ssh/config $SSH_DIR/config \
-    && read -p "Edit ssh config now ? (y/N) " && [[ $REPLY =~ ^[Yy]$ ]] \
-    && vim $SSH_DIR/config
+echo "... setup ssh config"
+privDir=$HOME/stuff/private
+if [ -d "$privDir" ]; then
+  [ -L "${sshDir}/config" ] && rm -v "${sshDir}/config"
+  [ -f ${sshDir}/config ] \
+    && echo 'config already exists' \
+    || cp ${privDir}/ssh/config ${sshDir}/config
+  ln -sfn ${privDir}/ssh/config.d ${sshDir}/config.d
 else
   echo "skipped, private repo missing"
 fi
 
-chmod -f 600 $SSH_DIR/*
-chmod -f 644 $SSH_DIR/*.pub
-chmod -f 700 $SSH_DIR/config.d/
+chmod -f 600 ${sshDir}/*
+chmod -f 644 ${sshDir}/*.pub
+chmod -f 700 ${sshDir}/config.d/
