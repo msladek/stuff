e2a8f6dc9e08c95af454ac742db42c6dcbc2d502
Marc Sladek <marc@sladek.me>
2022-01-31 23:56:47 +0100

    improve tmux setup

--- etc/user/profile/tmux.sh
+++ etc/user/profile/tmux.sh
@@ -1,18 +1,21 @@
-# always open up tmux session 'main' if
-# 1. tmux is available
-# 2. we are in a interactive terminal
-# 3. we aren't within screen
-# 4. we aren't already within tmux
-command -v tmux >/dev/null \
-  && [ -n "$PS1" ] && [[ $- == *i* ]] \
-  && [[ ! "$TERM" =~ screen ]] \
-  && [[ ! "$TERM" =~ tmux ]] \
-  && [ -z "$TMUX" ] \
-  && exec tmux new-session -A -s main
-
-[ "$TERM_PROGRAM" = tmux ] \
-  && alias ssh='TERM=xterm-256color ssh'
-
-[ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] \
-  && command -v neofetch >/dev/null \
-  && neofetch
+if command -v tmux >/dev/null; then
+  # check if already in tmux
+  if [ "$TERM_PROGRAM" = tmux ]; then
+    # for nested tmux don't let the remote host know about local tmux
+    alias ssh='TERM=xterm-256color ssh'
+    # run neofetch within tmux if we're remote
+    [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] \
+      && command -v neofetch >/dev/null \
+      && neofetch
+  elif [ -n "$PS1" ] && [[ $- == *i* ]] \
+    && [[ ! "$TERM" =~ screen ]] \
+    && [ ! "$TERM_PROGRAM" = vscode ]; then
+    # open/attach tmux session 'main' if
+    # 1. tmux is installed
+    # 2. we aren't already within tmux
+    # 3. we are in a interactive terminal
+    # 4. we aren't within screen
+    # 5. we aren't within vscode
+    exec tmux new-session -A -s main
+  fi
+fi
