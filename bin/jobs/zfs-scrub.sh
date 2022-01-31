6c78bab4dd97d16e5eb23b653a59d76f83a1e0b6
Marc Sladek <marc@sladek.me>
2022-02-01 00:18:23 +0100

    delete superfluous jobs

--- bin/jobs/zfs-scrub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e "Subject: [INFO] ZFS scrub started on $(hostname)\n\n`/sbin/zpool status`" | msmtp root@sladek.co
-for pool in $(/sbin/zpool list -Ho name); do
-  /sbin/zpool scrub $pool
-done
-
