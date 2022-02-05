af437e469213c9e6c4f8593a694fcaee649cf9d8
Marc Sladek <marc@sladek.me>
2022-02-05 15:14:23 +0100

    bash-completion for aliases

--- etc/user/profile/aliases.sh
+++ etc/user/profile/aliases.sh
@@ -11,10 +11,6 @@ alias ln='ln -i'
 alias sudo='sudo '
 alias apt="sudo apt"
 alias aptitude="sudo aptitude"
-command -v doas >/dev/null \
-  && alias sudo='doas ' \
-  && alias apt='doas apt' \
-  && alias aptitude='doas aptitude'
 
 # some more ls aliases
 command -v lsd >/dev/null \
@@ -60,31 +56,65 @@ command -v screen >/dev/null \
 command -v tmux >/dev/null \
   && alias tmuxr='tmux attach || tmux new-session' \
   && alias screen='tmux' \
-  && alias screenr='tmuxr'
+  && alias screenr='tmuxr' \
+# for nesting tmux don't let the remote host know about local tmux
+[ "$TERM_PROGRAM" = tmux ] || [ -n "$TMUX" ] \
+  && alias ssh='TERM=xterm-256color ssh'
 
-# systemctl aliases
+# systemctl aliases / completion
 alias sc='sudo SYSTEMD_EDITOR=vim systemctl'
 alias scs='systemctl status'
 alias scl='systemctl list-unit-files'
 alias sclu='systemctl list-units'
 alias sclt='systemctl list-timers --all'
+source /usr/share/bash-completion/completions/systemctl \
+  && complete -F _systemctl sc
 
-# git aliases
-command -v git >/dev/null \
-  && alias gia='git add' \
-  && alias gib='git branch' \
-  && alias gic='git commit' \
-  && alias gid='git diff' \
-  && alias gii='git init' \
-  && alias gim='git merge' \
-  && alias gio='git checkout' \
-  && alias gip='git pull' \
-  && alias gir='git reset' \
-  && alias gis='git status' \
-  && alias giu='git push -u' \
+# journalctl aliases / completion
+alias jc='sudo journalctl'
+source /usr/share/bash-completion/completions/journalctl \
+  && complete -F _journalctl jc
 
-# zfs aliases
-command -v zfs >/dev/null \
-  && alias zl='zfs list' \
-  && alias zla='zfs list -t all' \
-  && alias zls='zfs list -t snapshot'
+# git aliases / completion
+if command -v git >/dev/null; then
+  alias gia='git add'
+  alias gib='git branch'
+  alias gic='git commit'
+  alias gid='git diff'
+  alias gif='git fetch'
+  alias gii='git init'
+  alias gil='git log'
+  alias gim='git merge'
+  alias gio='git checkout'
+  alias gip='git pull'
+  alias gir='git reset'
+  alias gis='git status'
+  alias giu='git push -u'
+  alias gicu='git commit && git push -u'
+  source /usr/share/bash-completion/completions/git \
+    && __git_complete gia _git_add \
+    && __git_complete gib _git_branch \
+    && __git_complete gic _git_commit \
+    && __git_complete gid _git_diff \
+    && __git_complete gif _git_fetch \
+    && __git_complete gii _git_init \
+    && __git_complete gil _git_log \
+    && __git_complete gim _git_merge \
+    && __git_complete gio _git_checkout \
+    && __git_complete gip _git_pull \
+    && __git_complete gir _git_reset \
+    && __git_complete gis _git_status \
+    && __git_complete giu _git_push \
+    && __git_complete gicu _git_push
+fi
+
+# zfs aliases / completion
+if command -v zfs >/dev/null; then
+  alias zl='zfs list'
+  alias zla='zfs list -t all'
+  alias zls='zfs list -t snapshot'
+  source /usr/share/bash-completion/completions/zfs \
+    && complete -F __zfs_complete zl \
+    && complete -F __zfs_complete zla \
+    && complete -F __zfs_complete zls
+fi
