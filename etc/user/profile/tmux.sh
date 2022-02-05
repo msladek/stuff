1027f132e94e0f84ccedb01a86228399a9500c14
Marc Sladek <marc@sladek.me>
2022-02-05 14:56:38 +0100

    execute motd within tmux

--- etc/user/profile/tmux.sh
+++ etc/user/profile/tmux.sh
@@ -1,12 +1,14 @@
 if command -v tmux >/dev/null; then
-  # check if already in tmux
-  if [ "$TERM_PROGRAM" = tmux ]; then
-    # for nested tmux don't let the remote host know about local tmux
-    alias ssh='TERM=xterm-256color ssh'
-    # run neofetch within tmux if we're remote
+  # check if already in tmux, TERM_PROGRAM set in 3.2+
+  # https://github.com/tmux/tmux/commit/4e053685df9f4d1c398148712ab0529a7e9d32e7
+  if [ "$TERM_PROGRAM" = tmux ] || [ -n "$TMUX" ]; then
+    # run motd within tmux if we're remote
     [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] \
-      && command -v neofetch >/dev/null \
-      && neofetch
+      && run-parts /etc/update-motd.d \
+      && last --time-format=iso $USER \
+       | grep 'pts' | egrep -v "tmux|:S"
+       | head -n2 | tail -n1 \
+       | awk {'print "Last login: " $4 " from " $3'}
   elif [ -n "$PS1" ] && [[ $- == *i* ]] \
     && [[ ! "$TERM" =~ screen ]] \
     && [ ! "$TERM_PROGRAM" = vscode ]; then
