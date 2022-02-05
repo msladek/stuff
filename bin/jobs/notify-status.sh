f37c9c262f5e748b1afabf448e45ad7e73573979
Marc Sladek <marc@sladek.me>
2022-02-05 19:18:15 +0100

    fix param escape

--- bin/jobs/notify-status.sh
+++ bin/jobs/notify-status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 subject="[$(hostname)] systemd unit status"
 systemctl cat "${1}" &> /dev/null ] \
-  && message="$(systemctl status \"${1}\")" \
+  && message="$(systemctl status ${1})" \
   || message="called with unknown service name \"${1}\""
 echo -e "Subject: ${subject}\n\n${message}" | sendmail root
