8b148f738fdd77ab2d897bc6ce293cca56aeaa43
Marc Sladek <marc@sladek.dev>
2022-07-04 12:53:56 +0200

    shellcheck enpass.sh

--- etc/user/profile/enpass.sh
+++ etc/user/profile/enpass.sh
@@ -1,9 +1,8 @@
 if command -v enpasscli >/dev/null; then
 
-upid="/tmp/upid"
-[ ! -f "$upid" ] \
-  && install -T -m 600 /dev/null $upid \
-  && openssl rand -base64 32  > $upid
+[ ! -f /tmp/upid ] \
+  && install -T -m 600 /dev/null /tmp/upid \
+  && openssl rand -base64 32  > /tmp/upid
 
 export enp_vault=~/.enpasscli/vault
 export enp_keyfile=~/.enpasscli/keyfile
@@ -18,24 +17,24 @@ function enp() {
   enp_params="-pin -vault=${enp_vault} -keyfile=${enp_keyfile}"
   local pin="$ENP_PIN"
   [ -z "$pin" ] && read -s -p "Enter PIN: " pin && echo 1>&2
-  ENP_PIN=$pin ENP_PIN_PEPPER=$(cat ${upid}) enpasscli $enp_params "$@"
+  ENP_PIN=$pin ENP_PIN_PEPPER=$(cat /tmp/upid) enpasscli $enp_params "$@"
   local exit_code=$?
   [ $exit_code -eq 0 ] && [ -z "$ENP_PIN" ] && [ -n "$pin" ] \
     && export ENP_PIN=$pin \
     && trap 'unset ENP_PIN' SIGUSR1 \
     && local current_pid=$BASHPID \
-    && (( sleep 300; kill -SIGUSR1 $current_pid ) & ) > /dev/null 2>&1
+    && ( ( sleep 300; kill -SIGUSR1 $current_pid ) & ) > /dev/null 2>&1
   return $exit_code
 }
 
 if command -v enpass-askpass >/dev/null; then
-function ssh() { ssh-enp $@; }
+function ssh() { ssh-enp "$@"; }
 function ssh-enp() {
   local require=never
   ! ssh-add-has "${@: -1}" && enp dryrun && require=force
   SSH_ASKPASS=enpass-askpass SSH_ASKPASS_REQUIRE=$require command ssh "$@"
 }
-function ssh-add() { ssh-enp-add $@; }
+function ssh-add() { ssh-enp-add "$@"; }
 function ssh-enp-add() {
   local require=never
   ! ssh-add-has "${@: -1}" && enp dryrun && require=force
