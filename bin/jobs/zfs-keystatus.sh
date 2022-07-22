28d2ad464efc4c4305addb0720a8833dc0f6425e
Marc Sladek <marc@sladek.dev>
2022-07-23 00:37:39 +0200

    jobs.sh naming & SC2046

--- bin/jobs/zfs-keystatus.sh
+++ bin/jobs/zfs-keystatus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dataset="$1"
+dataset="${1/_/\/}"
 [ -z "$dataset" ] && echo 'no dataset provided' && exit 1
 
 keystatus=$(/sbin/zfs get keystatus -H -o value "$dataset")
--- bin/setup/tasks/jobs.sh
+++ bin/setup/tasks/jobs.sh
@@ -10,32 +10,36 @@ etcHostDir=/opt/msladek/stuffp/etc/$(hostname)
 
 # make the file immutable by other users, sets sticky bit on parent dir
 function claim() {
-  [ ! -z $1 ] && [ -f $1 ] \
-    && chown $USER $(dirname $1) && chmod 1775 $(dirname $1) \
-    && chown $USER $1 && chmod 644 $1 \
-    && { [[ $1 != *.sh ]] || chmod +x $1; }
+  [ -n "$1" ] && [ -f "$1" ] \
+    && chown "$USER" "$(dirname "$1")" && chmod 1775 "$(dirname "$1")" \
+    && chown "$USER" "$1" && chmod 644 "$1" \
+    && { [[ $1 != *.sh ]] || chmod +x "$1"; }
 }
 
-function install() {
+function installFile() {
   claim "$1" && ln -sf "$1" "$2"
 }
 
-function installUnit {
+function installUnit() {
   for i in $1; do
     [[ $i =~ \.service$ ]] \
-      && script=$(grep "^ExecStart=.*\.sh" $i | cut -c11-) \
-      && claim $script
-    install "$i" /etc/systemd/system/
+      && script=$(grep "^ExecStart=.*\.sh" "$i" | cut -c11-) \
+      && claim "$script"
+    installFile "$i" /etc/systemd/system/
   done
 }
 
-function installTimer() {
-  [ -d "$(dirname $1)" ] \
+function installTimedService() {
+  [ -d "$(dirname "$1")" ] \
     && installUnit "${1}*.service" \
     && installUnit "${1}*.timer" \
-    && systemctl daemon-reload \
+    && systemctl daemon-reload
+}
+
+function installEnableTimedService() {
+  installTimedService "$1" \
     && for timer in ${1}*.timer; do \
-        systemctl enable $(basename $timer); \
+        systemctl enable "$(basename "$timer")"; \
        done \
     && echo "done" || { echo "failed" && false; }
 }
@@ -44,16 +48,16 @@ echo "... notify status"
 installUnit $unitDir/notify-status@.service
 
 echo "... hosts file update"
-installTimer $unitDir/hosts-update \
+installEnableTimedService $unitDir/hosts-update \
   && rm -f /etc/cron.weekly/hosts-update
 
 echo "... filesytem trim"
-installTimer $unitDir/fstrim \
+installEnableTimedService $unitDir/fstrim \
   && rm -f /etc/cron.weekly/fstrim
 
 echo "... OS sync"
 if [ -w /mnt/backup/os ]; then
-  installTimer $unitDir/os-rsync \
+  installEnableTimedService $unitDir/os-rsync \
     && rm -f /etc/cron.weekly/os-rsync
 else
   echo "skipped, /mnt/backup/os not linked"
@@ -61,30 +65,31 @@ fi
 
 echo "... smart attribute dump"
 if [ -w /mnt/backup/smart ]; then
-  installTimer $unitDir/smart-dump
+  installEnableTimedService $unitDir/smart-dump
 else
   echo "skipped, /mnt/backup/smart not linked"
 fi
 
 echo "... database dump"
 if [ -w /mnt/backup/mysql ] && [ -f /etc/mysql/debian.cnf ]; then
-  installTimer $unitDir/mysql-dump
+  installEnableTimedService $unitDir/mysql-dump
 else
   echo "skipped, /mnt/backup/mysql not linked"
 fi
 
-if ! command -v zpool > /dev/null || [ $(zpool list -H | wc -l) -eq 0 ]; then
+if ! command -v zpool > /dev/null || [ "$(zpool list -H | wc -l)" -eq 0 ]; then
   echo "skipped zfs setup, no pools available"
 else
   echo "... zfs health check"
-  installTimer $unitDir/zfs-health \
+  installEnableTimedService $unitDir/zfs-health \
     && rm -f /etc/cron.hourly/zfs-health
 
-  echo "... zfs keystatus"
+  echo "... zfs keystatus" \
+  installTimedService $unitDir/zfs-keystatus
   for dataset in $(zfs get encryptionroot -H -ovalue -tfilesystem | uniq); do
     zfs list -H -o name | grep -qF -- "${dataset}" \
-      && echo -n "${dataset} ... " \
-      && installTimer $unitDir/zfs-keystatus@${dataset}
+      && echo "${dataset}" \
+      && systemctl enable "zfs-keystatus@${dataset/\//_}.timer"
   done
 
   echo "... sanoid"
@@ -92,7 +97,7 @@ else
     mkdir -p /etc/sanoid
     [ ! -f /etc/sanoid/sanoid.defaults.conf ] \
       && ln -s /usr/share/sanoid/sanoid.defaults.conf /etc/sanoid/sanoid.defaults.conf
-    install $etcHostDir/sanoid.conf /etc/sanoid/sanoid.conf \
+    installFile "$etcHostDir/sanoid.conf /etc/sanoid/sanoid.conf" \
       && systemctl enable sanoid.timer \
       && rm -f /etc/cron.d/sanoid
   else
@@ -101,7 +106,7 @@ else
 
   echo "... syncoid"
   if command -v syncoid > /dev/null; then
-    installTimer "$etcHostDir/systemd/syncoid"
+    installEnableTimedService "$etcHostDir/systemd/syncoid"
   else
     echo "skipped, syncoid not available"
   fi
