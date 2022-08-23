ee13cf397417ce8122776cf9e85c288b80272908
Marc Sladek <marc@sladek.dev>
2022-08-23 23:11:07 +0200

    automate key setup

--- bin/setup/tasks/git-msladek.sh
+++ bin/setup/tasks/git-msladek.sh
@@ -9,11 +9,15 @@ git config --global user.name "Marc Sladek"
 git config --global user.email "marc@sladek.dev"
 
 githubSign=~/.ssh/github_sign
+[ ! -f $githubSign ] \
+  && echo "no signing key found, generating one..." \
+  && ssh-keygen -t ed25519 -a 100 -f "$githubSign" -C "$(git config --get user.email)" \
+  && echo -e "add signing key to github:\n$(cat ${githubSign}.pub)"
 [ -f "$githubSign" ] && [ -f "${githubSign}.pub" ] \
   && git config --global gpg.format ssh \
-  && git config --global user.signingkey "$(cat "${githubSign}.pub")" \
+  && git config --global user.signingkey "$(cat ${githubSign}.pub)" \
   && git config --global commit.gpgsign true \
   && git config --global tag.gpgsign true \
-  || echo "WARNING: ssh key not found at ${githubSign}, please retry with priv/pub keys inplace."
+  || echo "WARNING: setup signing failed"
 
 exit 0
--- bin/setup/tasks/git.sh
+++ bin/setup/tasks/git.sh
@@ -6,9 +6,13 @@ echo -e "\nSetup Git config ..."
   && exit 1
 
 git config --global core.autocrlf input
-git config --global core.editor "vim"
+git config --global core.editor vim
 git config --global pull.rebase false
 git config --global push.default current
+[ -d /opt/msladek/stuff ] \
+  && ! git config --get safe.directory | grep -qF "/opt/msladek/stuff" \
+  && git config --global --add safe.directory "/opt/msladek/stuff"
+
 if [ -f ~/.ssh/github ]; then
   git config --global core.sshCommand "ssh -i ~/.ssh/github -F /dev/null"
   git -C /opt/msladek/stuff remote set-url origin git@github.com:msladek/stuff.git > /dev/null 2>&1
