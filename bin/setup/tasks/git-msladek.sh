ffe22b6bdd94dd7f1910cf25a040771f0778ec06
Marc Sladek <marc@sladek.dev>
2022-08-23 22:42:57 +0200

    switch to ssh key signing

--- bin/setup/tasks/git-msladek.sh
+++ bin/setup/tasks/git-msladek.sh
@@ -7,12 +7,13 @@ echo -e "Setup Git config for msladek ..."
 
 git config --global user.name "Marc Sladek"
 git config --global user.email "marc@sladek.dev"
-githubGPG=~/.ssh/github_gpg
-[ -f "$githubGPG" ] \
-  && gpg --import "${githubGPG}.pub" \
-  && gpg --import "$githubGPG" \
-  && git config --global user.signingkey 8C64BE4EC6D00407 \
+
+githubSign=~/.ssh/github_sign
+[ -f "$githubSign" ] && [ -f "${githubSign}.pub" ] \
+  && git config --global gpg.format ssh \
+  && git config --global user.signingkey "$(cat "${githubSign}.pub")" \
   && git config --global commit.gpgsign true \
-  || echo "WARNING: gpg keys not found at ${githubGPG}, please retry with priv/pub keys inplace."
+  && git config --global tag.gpgsign true \
+  || echo "WARNING: ssh key not found at ${githubSign}, please retry with priv/pub keys inplace."
 
 exit 0
