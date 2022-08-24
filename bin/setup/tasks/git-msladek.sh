1b5c2d31f5c3d11b813364d6dcbcc2f9baaf8447
Marc Sladek <marc@sladek.dev>
2022-08-24 18:41:52 +0200

    improve ssh key setup

--- bin/setup/tasks/git-msladek.sh
+++ bin/setup/tasks/git-msladek.sh
@@ -8,11 +8,20 @@ echo -e "Setup Git config for msladek ..."
 git config --global user.name "Marc Sladek"
 git config --global user.email "marc@sladek.dev"
 
+githubAuth=~/.ssh/github
+githubAuthFile=~/.ssh/github_$(hostname)
+[ ! -f $githubAuth ] \
+  && echo "no auth key found, generating one..." \
+  && ssh-keygen -t ed25519 -a 100 -f "${githubAuthFile}" -C "$(git config --get user.email)" \
+  && ln -sf "${githubAuthFile}"     "${githubAuth}" \
+  && ln -sf "${githubAuthFile}.pub" "${githubAuth}.pub" \
+  && echo -e "\n[NOTE] add auth key to github: $(cat ${githubAuth}.pub)\n"
+
 githubSign=~/.ssh/github_sign
 [ ! -f $githubSign ] \
   && echo "no signing key found, generating one..." \
   && ssh-keygen -t ed25519 -a 100 -f "$githubSign" -C "$(git config --get user.email)" \
-  && echo -e "add signing key to github:\n$(cat ${githubSign}.pub)"
+  && echo -e "\n[NOTE] add signing key to github:\n$(cat ${githubSign}.pub)\n"
 [ -f "$githubSign" ] && [ -f "${githubSign}.pub" ] \
   && git config --global gpg.format ssh \
   && git config --global user.signingkey "$(cat ${githubSign}.pub)" \
--- bin/setup/user-desktop.sh
+++ bin/setup/user-desktop.sh
@@ -5,8 +5,8 @@ taskDir=$setupDir/tasks
 bash "$taskDir/profile.sh"
 bash "$taskDir/private.sh"
 bash "$taskDir/ssh.sh"
-bash "$taskDir/git.sh"
 bash "$taskDir/git-msladek.sh"
+bash "$taskDir/git.sh"
 bash "$taskDir/desktop-setup.sh"
 
 echo -e "\n... done!"
