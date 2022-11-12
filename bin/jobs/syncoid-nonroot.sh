806f9c06c6a3368f1f15abb512db6b52a1653c6f
Marc Sladek <marc@sladek.dev>
2022-11-12 03:31:17 +0100

    merge syncoid scripts

--- bin/jobs/syncoid-nonroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "${@:(-2):1} -> ${@:(-1):1}"
-/usr/sbin/syncoid \
-  --no-privilege-elevation \
-  --no-sync-snap \
-  $@
--- bin/jobs/syncoid.sh
+++ bin/jobs/syncoid.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "${@:(-2):1} -> ${@:(-1):1}"
+args=( "$@" )
+[ "$1" == "-b" ] && args=( "--create-bookmark" "${@:2}" )
+echo "${args[@]}"
 /usr/sbin/syncoid \
   --no-privilege-elevation \
   --no-sync-snap \
-  --create-bookmark \
-  --recursive \
   --skip-parent \
-  $@
+  "${args[@]}"
