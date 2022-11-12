39dcbf07d38a0a69eec441611459083599736743
Marc Sladek <marc@sladek.dev>
2022-11-12 03:54:06 +0100

    remove echo

--- bin/jobs/syncoid.sh
+++ bin/jobs/syncoid.sh
@@ -2,7 +2,6 @@
 echo "${@:(-2):1} -> ${@:(-1):1}"
 args=( "$@" )
 [ "$1" == "-b" ] && args=( "--create-bookmark" "${@:2}" )
-echo "${args[@]}"
 /usr/sbin/syncoid \
   --no-privilege-elevation \
   --no-sync-snap \
